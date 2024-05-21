cff1face1464f1f35170da59274fbfba2a4e64df
ShotaMiura117 <syouta-miura@allow.co.jp>
2024-05-21 13:35:12 +0900

    ログインサーブレット作成
    jspで入力された情報の取得

--- WEB-INF/src/food_management/LoginServlet.java
+++ WEB-INF/src/food_management/LoginServlet.java
@@ -1,5 +1,36 @@
 package food_management;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        // ここでユーザー名とパスワードを検証する処理を追加します。
+        // 仮の検証を行う場合は、以下のコードをコメントアウトしてください。
+        /*
+        if ("admin".equals(username) && "admin".equals(password))
+        {
+            response.sendRedirect("success.jsp");
+        }
+        else
+        {
+            response.sendRedirect("failure.jsp");
+        }
+        */
+    }
+}
+
+/*
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -11,16 +42,18 @@ public class LoginServlet extends HttpServlet
 	{
 		request.setCharacterEncoding("Windows-31J");
 
-		String id = request.getParameter("id");
+		String id = request.getParameter("email");
 		String password = request.getParameter("password");
 
-		/*
+
 		response.setCharacterEncoding("Windows-31J");
 		PrintWriter out = response.getWriter();
 		out.println("ログインしました<br>");
 		out.println("入力された情報を表示します<br>");
 		out.println("ID : " + id + "    PASSWORD : " + password);
-		*/
+
+
 	}
 
 }
+*/
