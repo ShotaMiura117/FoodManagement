3df5f70a1f04a014ac071c0b31eb9df226807b45
ShotaMiura117 <syouta-miura@allow.co.jp>
2024-05-23 16:06:10 +0900

    LoginBean・LoginDAOの作成
    データベース上の情報を取り込むための準備

--- WEB-INF/src/food_management/LoginBean.java
+++ WEB-INF/src/food_management/LoginBean.java
@@ -1,5 +1,45 @@
 package food_management;
 
-public class LoginBean {
+public class LoginBean
+{
+	private String pkid;
+	private String userid;
+	private String password;
+
+
+	public LoginBean() {
+	}
+
+	public LoginBean(String pkid, String userid, String password)
+	{
+		this.pkid = pkid;
+		this.userid = userid;
+		this.password = password;
+	}
+
+	public String getPkid()
+	{
+		return pkid;
+	}
+	public void setPkid(String pkid)
+	{
+		this.pkid = pkid;
+	}
+	public String getUserid()
+	{
+		return userid;
+	}
+	public void setUserid(String userid)
+	{
+		this.userid = userid;
+	}
+	public String getPassword()
+	{
+		return password;
+	}
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
 
 }
---
+++ WEB-INF/src/food_management/LoginDAO.java
@@ -0,0 +1,20 @@
+package food_management;
+
+import java.sql.*;
+import java.util.*;
+
+public class LoginDAO {
+
+	/*
+	public static int insert(LoginBean cbean) throws SQLException
+	{
+		String sql = "INSERT INTO CUSTOMER" +
+		"(PKID, USERID, PASSWORD) VALUES (" +
+		"'" + cbean.getPkid() + "', " +
+		"'" + cbean.getUserid() + "', " +
+		cbean.getPassword() + ")";
+		return DBManager.simpleUpdate(sql);
+	}
+	*/
+
+}
